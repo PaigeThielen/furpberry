0ddd5374dca27e033d82c0e44952c06a3164bdb6
PaigeThielen <31465090+PaigeThielen@users.noreply.github.com>
2024-09-02 20:17:29 -0500

    Motor, light detection, image display on two screens. cleanup update requirements remove extra files

--- src/furpberry/main.py
+++ src/furpberry/main.py
@@ -1,10 +1,79 @@
-from furpberry.util.motor import Motor
 import time
+from random import randint
+
+import RPi.GPIO as GPIO
+from importlib_resources import files
+from PIL import Image
+
+import furpberry.util.img
+from src.furpberry.util.hardware import Display, LightSense, Motor
+
+
+class Furby:
+    def __init__(
+        self,
+        x_offset: int = 0,
+        y_offset: int = 0,
+        height: int = 240,
+        width: int = 240,
+        rotation: int = 90,
+        invert: bool = False,
+    ) -> None:
+        self.eye_height = height
+        self.eye_width = width
+        self._rotation = rotation
+        self.left_eye: Display = Display(0, x_offset, y_offset, height, width, rotation, invert)
+        self.right_eye: Display = Display(1, x_offset, y_offset, height, width, rotation, invert)
+        self.light_sensor = LightSense()
+        self.motor = Motor()
+        self.images: list[str] = sorted(files(furpberry.util.img))
+        self.starting_image_index: int = -1
+
+    def open_eyes(self, starting_image_index: int) -> None:
+        # crop image into two images
+        # display left half on left eye
+        # display right_eye half on right_eye eye
+        self.starting_image_index = starting_image_index
+        image_original = Image.open(self.images[starting_image_index])
+        image_resized = image_original.resize((self.eye_width * 2, self.eye_height))
+        left_crop = image_resized.crop((0, 0, self.eye_width, self.eye_height))
+        right_crop = image_resized.crop((0 + self.eye_width, 0, self.eye_width * 2, self.eye_height))
+
+        self.left_eye.open_eye(left_crop)
+        self.right_eye.open_eye(right_crop)
+
+    def close_eyes(self) -> None:
+        self.left_eye.close_eye()
+        self.right_eye.close_eye()
+
+    def roll_eyes(self):
+        self.open_eyes(self.starting_image_index)
+        self.starting_image_index += 1
+        time.sleep(0.1)
+
+    def wake_up_and_dance(self):
+        # move motor and
+        # cycle through eyeball images
+        starting_eyes_index = randint(0, len(self.images) - 1)
+        self.motor.start()
+        self.open_eyes(starting_eyes_index)
+        while self.light_sensor.measure():
+            self.roll_eyes()
+        self.close_eyes()
+        self.motor.stop()
+
+
+def run_furby():
+    furby = Furby()
+    try:
+        while True:
+            if furby.light_sensor.measure():
+                furby.wake_up_and_dance()
+            else:
+                time.sleep(0.1)
+    except KeyboardInterrupt:
+        pass
+
 
 if __name__ == "__main__":
-	motor = Motor()
-	motor.wiggle(5)
-	#motor.set_clockwise()
-	#motor.start()
-	#time.sleep(3)
-	#motor.stop()
+    run_furby()
